eb187529a288e98c8a407fe744317106c8ecb238
davm07 <diegovasquezmejia1999@gmail.com>
2024-06-12 14:45:59 +0200

    finish part 1 project

---
+++ db/database.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+let db;
+
+const initDb = (callback) => {
+    if(db) {
+        console.log('Database already initialized');
+        return callback(null, db);
+    }
+
+    MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+        db = client
+        callback(null, db);
+        })
+    .catch((err) => { 
+        callback(err, null);
+        });
+}
+
+const getDb = () => {
+    if(!db) {
+        throw new Error('Database not initialized');
+    }
+    return db;
+}
+
+module.exports = {
+    initDb,
+    getDb
+}
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+
+const mongodb = require('./db/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes/index'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`);
+          });
+    }
+})
